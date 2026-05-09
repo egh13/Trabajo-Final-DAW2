a383d7c57d2079994fb38be228b3b64c9de35eb8
egh13 <ericgarciahendricks@gmail.com>
2026-05-09 13:20:19 +0200

    Update rutas de categoria, disponibles solo para admin

--- backend/src/routes/categoryRoutes.ts
+++ backend/src/routes/categoryRoutes.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express'
 import { getCategories, getCategoryById, createCategory, updateCategory, deleteCategory } from '../controllers/categoryController'
+import { authorize } from '../middlewares/authMiddleware'
 
 const router = Router()
 
 router.get('/', getCategories)
 router.get('/:id', getCategoryById)
-router.post('/', createCategory)
-router.put('/:id', updateCategory)
-router.delete('/:id', deleteCategory)
+// Rutas admin
+router.post('/', authorize('admin'), createCategory)
+router.put('/:id', authorize('admin'), updateCategory)
+router.delete('/:id', authorize('admin'), deleteCategory)
 
 export default router
